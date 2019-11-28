f7f6057f290e0ee7db2c5e9d71c4f2d62e68ffb0
Sahil Singh <sahil.dogstar@gmail.com>
2019-11-28 13:52:00 +0530

    Finished Day 40 Morning Challenge

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,85 @@
 // Challenge
 // Find the best move given the state of Towers of Hanoi board
 
+List<List<int>> willWinInOneMove(List<List<int>> inputState) {
+  List<List<List<int>>> allPossibleMoves = possibleMoves(inputState);
+  for (int i = 0; i < allPossibleMoves.length - 1; i++) {
+      if(checkForFinalState(allPossibleMoves[i])){
+        return allPossibleMoves[i];
+      }
+          
+  }
+}
+
+bool checkForFinalState(List<List<int>> listToCheck){
+  if(listToCheck[0].isEmpty && listToCheck[1].isEmpty){
+  return true;
+  }
+  return false;
+}
+
+List<List<int>> willWinInTwoMoves(List<List<int>> inputState){
+  List<List<List<int>>> allPossibleMoves = possibleMoves(inputState);
+  for(List<List<int>> list in allPossibleMoves){
+    if(willWinInOneMove(list) != null){
+      return list;
+    }
+  }
+  return null;
+}
+
+List<List<List<int>>> possibleMoves(List<List<int>> inputState) {
+  List<List<List<int>>> finalState = [];
+
+  for (int i = 0; i < inputState.length; i++) {
+    for (int j = 0; j < inputState.length; j++) {
+      if (isValidMove(inputState, i, j)) {
+        List<List<int>> copyInputState = [];
+        for (List<int> list in inputState) {
+          List<int> tempList = List.from(list);
+          copyInputState.add(tempList);
+        }
+        copyInputState[j].insert(0, copyInputState[i][0]);
+
+        copyInputState[i].removeAt(0);
+
+        finalState.add(copyInputState);
+      }
+    }
+  }
+  return finalState;
+}
+
+bool isValidMove(List<List<int>> checkList, int from, int to) {
+  if (checkList[from].isEmpty) {
+    return false;
+  }
+
+  if (checkList[to].isEmpty) {
+    return true;
+  }
+
+  return (checkList[from].first < checkList[to].first);
+}
+
 main() {
+  print(possibleMoves([
+    [1],
+    [],
+    [2, 3, 4]
+  ]));
+   print(willWinInTwoMoves([
+    [1],
+    [2],
+    [3, 4]
+  ]));
+  print(willWinInOneMove([
+    [1],
+    [],
+    [2, 3, 4]
+  ]));
+  // print(secondBestMove([[1],[2],[3,4]]));
+  // print(willFinishInTwoMoves([[2],[1],[3,4]]));
+  //print(possibleMoves([[2,3],[1],[4]]));
+  //print(bestMoves([[2,3], [1], [4,]]));
 }
--- test/Day_40_Morning_Challenge_test.dart
+++ test/Day_40_Morning_Challenge_test.dart
@@ -1,6 +1,10 @@
 import 'package:test/test.dart';
+import '../bin/main.dart';
 
 void main() {
   test('Test Case 1', () {
+    expect(willWinInOneMove([[1],[],[2,3,4]]), [[],[],[1,2,3,4]]);
+    expect(willWinInTwoMoves([[1,3],[],[4,2]]), null);
+    expect(willWinInTwoMoves([[1],[2],[3,4]]), [[1],[],[2,3,4]]);
   });
 }
